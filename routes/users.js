981243224abb43a8a1a2c3d9e3e775a501c5e29e
Tommy Mallis <tommymallis@gmail.com>
2020-01-02 12:22:36 -0800

    Commiting small change to rebase

--- routes/users.js
+++ routes/users.js
@@ -6,44 +6,45 @@ const checkAuth = require('../task-manager/src/middleware/check-auth');
 const UsersController = require('../task-manager/src/controller/users');
 
 /**Put you DB path here, you can use this default path to host it local at this address */
-mongoose.connect('', {
+mongoose.connect('mongodb://localhost:5000/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
 const redirectDashboard = (req, res, next) => {
-    if (req.session.userId) {
-        res.redirect('/users/login')
-    } else {
-        console.log('Session Id doesn\'t exist')
-        next()
-    }
-}
-
+  if (req.session.userId) {
+    res.redirect('/users/login');
+  } else {
+    console.log("Session Id doesn't exist");
+    next();
+  }
+};
 
 /*POST user signup, posting to /users/signup with json will create entry in DB */
-router.post('/signup', redirectDashboard, UsersController.user_sign_up);
+router.post('/signup', UsersController.user_sign_up);
 
 // Pretty much only used if session id still exist
 router.get('/login', (req, res) => {
-    res.render('dashboard')
-})
+  res.render('dashboard');
+});
 
 router.post('/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password )
-        console.log('user: ', user)
-        // const token = await User.generateAuthToken()
-        
-        req.session.userId = user._id
-        console.log('login session',req.session)
-        res.render('dashboard')
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    console.log('user: ', user);
+    // const token = await User.generateAuthToken()
 
-    }catch(e) {
-        console.log(e)
-        res.redirect('/?error=' + e)
-    }
+    req.session.userId = user._id;
+    console.log('login session', req.session);
+    res.render('dashboard');
+  } catch (e) {
+    console.log(e);
+    res.redirect('/?error=' + e);
+  }
 });
 
 // const auth = async (req, res, next) => {
@@ -63,9 +64,7 @@ router.post('/login', async (req, res) => {
 //     }
 // }
 
-
-// GET all displayNames! 
+// GET all displayNames!
 router.get('/', UsersController.user_display_names);
-
 
 module.exports = router;
