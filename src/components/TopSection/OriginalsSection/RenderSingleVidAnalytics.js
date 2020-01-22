cd2d80767cdd0fd9bc0f477aaf70bc6b0e99b1cc
Eddie Gomez <eddieegomez4@gmail.com>
2020-01-21 17:25:38 -0800

    bar graph shows views per day

--- src/components/TopSection/OriginalsSection/RenderSingleVidAnalytics.js
+++ src/components/TopSection/OriginalsSection/RenderSingleVidAnalytics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Scoring from './Scoring';
-import Graph from './Graph';
+// import Graph from './Graph';
 import { web } from '../../../../oauthTwo.keys.json';
 import runTheContent from '../../../helpers/GetToken';
 /*************************************Get todays date */
@@ -8,29 +8,35 @@ let dateObj = new Date();
 let month = String(dateObj.getMonth() + 1).padStart(2, '0');
 let day = String(dateObj.getDate()).padStart(2, '0');
 let year = dateObj.getFullYear();
-let output = year + '-' + month + '-' + day;
+let todaysDate = year + '-' + month + '-' + day;
+
 /**************************************************** */
 
 /**************************************************************************************/
 /************************* Component to get single videos analytics********************/
 /**************************************************************************************/
-
 class RenderSingleVidAnalytics extends React.Component {
   state = {
-    singleVideoAnalytics: {}
+    singleVideoAnalytics: {},
+    videoData: null
   };
 
+  componentDidMount() {
+    google.charts.load('current', { packages: ['corechart'] });
+  }
   componentDidUpdate(prevProps) {
     if (this.props.selectedVideoId !== prevProps.selectedVideoId) {
       this.getSingleVidAnalytics();
       this.getSearchTerms();
     }
   }
+
   /**Functions to get views and watch time for specific video, wrapped in Brads helper function to get accessToken**/
   getSingleVidAnalytics() {
-    runTheContent((accessToken) => {
+    runTheContent(accessToken => {
       fetch(
-        `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=day%2CinsightTrafficSourceType&endDate=${output}&filters=video==${this.props.selectedVideoId}&ids=channel%3D%3DMINE&metrics=views%2CestimatedMinutesWatched&sort=day%2C-views&startDate=2005-02-14&key=${web.apiKey}`,
+        `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=day&endDate=${todaysDate}&filters=video%3D%3D${this.props.selectedVideoId}&ids=channel%3D%3DMINE&metrics=views%2Ccomments%2Clikes%2Cdislikes%2CestimatedMinutesWatched%2CaverageViewDuration&startDate=2005-02-14&key=${web.apiKey}`,
+
         {
           method: 'GET',
           headers: {
@@ -39,15 +45,27 @@ class RenderSingleVidAnalytics extends React.Component {
           }
         }
       )
-        .then((response) => response.json())
+        .then(response => response.json())
         .then(
-          (response) => {
+          response => {
             let totalViews = response.rows
-              .map((row) => row[2])
+              .map(row => row[1])
               .reduce((a, b) => a + b);
 
             let totalestimatedMinutesWatched = response.rows
-              .map((row) => row[3])
+              .map(row => row[5])
+              .reduce((a, b) => a + b);
+
+            let totalComments = response.rows
+              .map(row => row[2])
+              .reduce((a, b) => a + b);
+
+            let totalLikes = response.rows
+              .map(row => row[3])
+              .reduce((a, b) => a + b);
+
+            let totalDislikes = response.rows
+              .map(row => row[4])
               .reduce((a, b) => a + b);
 
             this.setState({
@@ -55,23 +73,30 @@ class RenderSingleVidAnalytics extends React.Component {
                 day: response.rows.length,
                 insightTrafficSourceType: null,
                 views: totalViews,
+                comments: totalComments,
+                likes: totalLikes,
+                dislikes: totalDislikes,
                 estimatedMinutesWatched: totalestimatedMinutesWatched + ' min'
-              }
+              },
+              videoData: response.rows
             });
           },
 
           function(err) {
             console.error('Execute error', err);
           }
-        );
+        )
+        .then(() => {
+          google.charts.setOnLoadCallback(this.drawChart);
+        });
     });
   }
 
   /**function to get best keyword for your specific videos**/
   getSearchTerms() {
-    runTheContent((accessToken) => {
+    runTheContent(accessToken => {
       fetch(
-        `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=insightTrafficSourceDetail&endDate=${output}&filters=video%3D%3D${this.props.selectedVideoId}%3BinsightTrafficSourceType%3D%3DYT_SEARCH&ids=channel%3D%3DMINE&maxResults=10&metrics=views&sort=-views&startDate=2005-07-01&key=${web.apiKey}`,
+        `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=insightTrafficSourceDetail&endDate=${todaysDate}&filters=video%3D%3D${this.props.selectedVideoId}%3BinsightTrafficSourceType%3D%3DYT_SEARCH&ids=channel%3D%3DMINE&maxResults=10&metrics=views&sort=-views&startDate=2005-07-01&key=${web.apiKey}`,
         {
           method: 'GET',
           headers: {
@@ -80,9 +105,9 @@ class RenderSingleVidAnalytics extends React.Component {
           }
         }
       )
-        .then((response) => response.json())
+        .then(response => response.json())
         .then(
-          (response) => {
+          response => {
             if (!response.rows[0]) {
               this.setState({
                 keyWord: 'N/A'
@@ -102,9 +127,10 @@ class RenderSingleVidAnalytics extends React.Component {
 
   renderSingleVidAnalytics() {
     let {
-      //day,
-      //insightTrafficSourceType,
       views,
+      comments,
+      likes,
+      dislikes,
       estimatedMinutesWatched
     } = this.state.singleVideoAnalytics;
 
@@ -113,6 +139,9 @@ class RenderSingleVidAnalytics extends React.Component {
         <div>
           <h2>Traffic</h2>
           <p>Views: {views} </p>
+          <p>Comment: {comments}</p>
+          <p>Likes: {likes}</p>
+          <p>Dislikes: {dislikes} </p>
           <p>Estimated min watched: {estimatedMinutesWatched}</p>
           <p>Key search term: {this.state.keyWord}</p>
         </div>
@@ -122,12 +151,44 @@ class RenderSingleVidAnalytics extends React.Component {
     }
   }
 
+  drawChart = () => {
+    // Create the data table.
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Views');
+
+    this.state.videoData.map(record => data.addRows([[record[0], record[1]]]));
+
+    const options = {
+      title: 'Views/Days',
+      backgroundColor: 'transparent',
+      chartArea: { left: 0, top: 0, width: '100%', height: '100%' },
+      animation: {
+        duration: 1000,
+        easing: 'in',
+        startup: true
+      }
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    const chart = new google.visualization.BarChart(
+      document.getElementById('chart_div')
+    );
+    chart.draw(data, options);
+  };
+
   render() {
     return (
       <div className='basic-analytics-container'>
         <h2>Originals</h2>
         <div className='basic-analytics'>
-          <Graph videoData={this.props.videoData} />
+          {/* <Graph videoData={this.props.videoData} /> */}
+          {this.state.videoData ? (
+            <div id='chart_div'></div>
+          ) : (
+            <div>Click on a video to render some cool data</div>
+          )}
           {this.renderSingleVidAnalytics()}
           <Scoring />
         </div>
