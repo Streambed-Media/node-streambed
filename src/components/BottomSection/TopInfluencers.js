788d09ed5757375d13a3ddbfdc9d34c5d5cab0d6
Tommy Mallis <tommymallis@gmail.com>
2020-01-23 15:31:04 -0800

    Using randomuser generator to get dynmic data for top influencer section

--- src/components/BottomSection/TopInfluencers.js
+++ src/components/BottomSection/TopInfluencers.js
@@ -1,111 +1,51 @@
-import React from 'react';
-import Avatar from '../../../public/images/rachel.png';
+import React, { useState, useEffect } from 'react';
+//import Avatar from '../../../public/images/rachel.png';
 
 /*************************************Top influencers sections, non-functional currently */
 
 const TopInfluencers = () => {
+  /**User State */
+  const [users, setUsers] = useState([]);
+
+  /**Fetching fake users to simulate dynamic user data */
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=4')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.results);
+        console.log(data.results);
+      });
+  }, []);
+
   return (
     <div className='influencer-container'>
       <h1>Top Influencers / Sharers</h1>
       <div className='influencers'>
-        <div className='list--one'>
-          <div className='ui list'>
-            <div className='item'>
-              <img className='ui avatar image' src={Avatar} alt='' />
-              <div className='content'>
-                <a className='header' href='/'>
-                  Rachel
-                </a>
-                <div className='description'>
-                  Last seen watching{' '}
-                  <a href='/'>
-                    <b>Arrested Development</b>
-                  </a>{' '}
-                  just now.
+        {users
+          ? users.map((c) => (
+              <div className='ui list'>
+                <div className='item'>
+                  <img
+                    className='ui avatar image'
+                    src={c.picture.thumbnail}
+                    alt=''
+                  />
+                  <div className='content'>
+                    <a className='header' href='#'>
+                      {c.name.first}
+                    </a>
+                    <div className='description'>
+                      Last seen watching{' '}
+                      <a href='/'>
+                        <b>Arrested Development</b>
+                      </a>{' '}
+                      just now.
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='item'>
-              <img className='ui avatar image' src={Avatar} alt='' />
-              <div className='content'>
-                <a className='header' href='/'>
-                  Lindsay
-                </a>
-                <div className='description'>
-                  Last seen watching{' '}
-                  <a href='/'>
-                    <b>Bob's Burgers</b>
-                  </a>{' '}
-                  10 hours ago.
-                </div>
-              </div>
-            </div>
-            <div className='item'>
-              <img className='ui avatar image' src={Avatar} alt='' />
-              <div className='content'>
-                <a className='header' href='/'>
-                  Matthew
-                </a>
-                <div className='description'>
-                  Last seen watching{' '}
-                  <a href='/'>
-                    <b>The Godfather Part 2</b>
-                  </a>{' '}
-                  yesterday.
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='list--two'>
-          <div className='ui list'>
-            <div className='item'>
-              <img className='ui avatar image' src={Avatar} alt='' />
-              <div className='content'>
-                <a className='header' href='/'>
-                  Rachel
-                </a>
-                <div className='description'>
-                  Last seen watching{' '}
-                  <a href='/'>
-                    <b>Arrested Development</b>
-                  </a>{' '}
-                  just now.
-                </div>
-              </div>
-            </div>
-            <div className='item'>
-              <img className='ui avatar image' src={Avatar} alt='' />
-              <div className='content'>
-                <a className='header' href='/'>
-                  Lindsay
-                </a>
-                <div className='description'>
-                  Last seen watching{' '}
-                  <a href='/'>
-                    <b>Bob's Burgers</b>
-                  </a>{' '}
-                  10 hours ago.
-                </div>
-              </div>
-            </div>
-            <div className='item'>
-              <img className='ui avatar image' src={Avatar} alt='' />
-              <div className='content'>
-                <a className='header' href='/'>
-                  Matthew
-                </a>
-                <div className='description'>
-                  Last seen watching{' '}
-                  <a href='/'>
-                    <b>The Godfather Part 2</b>
-                  </a>{' '}
-                  yesterday.
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            ))
+          : null}
       </div>
     </div>
   );
