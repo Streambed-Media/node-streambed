a38b21668954dd3e8de73fb515a873a1737130f2
Tommy Mallis <tommymallis@gmail.com>
2020-01-23 14:15:48 -0800

    Changed button for Publisher or Analytics page

--- src/components/UserForms/PubAnalytics.js
+++ src/components/UserForms/PubAnalytics.js
@@ -2,23 +2,22 @@ import React, { useState } from 'react';
 
 const PubAnalytics = (props) => {
   /**Recieves functions from app component, used here to pass the onChange back up to App */
-
+  const [pub, setPub] = useState(false);
   const onCheckChange = () => {
     props.handleClose();
     props.checkboxHandler();
+    setPub(!pub);
   };
 
   return (
     <div>
-      <div className='ui toggle checkbox'>
-        <input
-          type='checkbox'
-          name='publisher-analytics'
-          checked={props.checked}
-          onChange={onCheckChange}
-        />
-        <label>Publisher / Analytics </label>
-      </div>
+      <button
+        id='pub--lish'
+        className='reset--pw'
+        onClick={() => onCheckChange()}
+      >
+        {pub ? 'Analytics' : 'Publisher'}
+      </button>
     </div>
   );
 };
