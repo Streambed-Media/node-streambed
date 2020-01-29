0de959191c1093a6ff7cce163efe3b12b7dc7fe1
Tommy Mallis <tommymallis@gmail.com>
2020-01-29 13:30:31 -0800

    Fixed everything after merging

--- task-manager/src/middleware/sendFlo.js
+++ task-manager/src/middleware/sendFlo.js
@@ -1,34 +1,37 @@
-require('dotenv').config()
+require('dotenv').config();
 const Rpc = require('bitcoin-rpc-async');
 
-const sendFlo = async ( floData ) => {
-    console.log('flodata', floData)
-    const rpcUser = 'user';
-    const rpcPass = 'pass';
-    const rpcHost = 'localhost';
-    const rpcPort = '7313' // 7313 for main net - 17313 for test net
-    const rpc = new Rpc(`http://${rpcUser}:${rpcPass}@${rpcHost}:${rpcPort}`)
+const sendFlo = async (floData) => {
+  console.log('flodata', floData);
+  const rpcUser = 'user';
+  const rpcPass = 'pass';
+  const rpcHost = '192.168.1.101';
+  const rpcPort = '7313'; // 7313 for main net - 17313 for test net
+  const rpc = new Rpc(`http://${rpcUser}:${rpcPass}@${rpcHost}:${rpcPort}`);
 
-    const streambedWalletAddress = 'FGbRhT3vTSTiBEe927nispTkNCYAfoYcrm'
- 
-    let paramObj = {'address': streambedWalletAddress, 'amount': 0.01, 'floData': floData}
-    let reply = await rpc.run('sendtoaddress', paramObj)
-    console.log(reply)
-    if (reply.error !== null) {
-        return `Uh oh... error: ${reply}`
-    }
-    if (reply.error) {
-        return `Uh oh... error: ${reply.error.message}`
-    }
-    console.log(`txid: ${reply.result}`)
-    console.log(
-        `http://network.flo.cash/tx/${reply.result} \n`+
-        `https://livenet.flocha.in/tx/${reply.result}\n`+
-        `https://testnet.flocha.in/tx/${reply.result}\n`+
-        `https://api.oip.io/oip/o5/record/get/${reply.result}`
-    )
-    return reply.result
-}
+  const streambedWalletAddress = 'FGbRhT3vTSTiBEe927nispTkNCYAfoYcrm';
 
-module.exports = { sendFlo }
+  let paramObj = {
+    address: streambedWalletAddress,
+    amount: 0.01,
+    floData: floData
+  };
+  let reply = await rpc.run('sendtoaddress', paramObj);
+  console.log(reply);
+  if (reply.error !== null) {
+    return `Uh oh... error: ${reply}`;
+  }
+  if (reply.error) {
+    return `Uh oh... error: ${reply.error.message}`;
+  }
+  console.log(`txid: ${reply.result}`);
+  console.log(
+    `http://network.flo.cash/tx/${reply.result} \n` +
+      `https://livenet.flocha.in/tx/${reply.result}\n` +
+      `https://testnet.flocha.in/tx/${reply.result}\n` +
+      `https://api.oip.io/oip/o5/record/get/${reply.result}`
+  );
+  return reply.result;
+};
 
+module.exports = { sendFlo };
