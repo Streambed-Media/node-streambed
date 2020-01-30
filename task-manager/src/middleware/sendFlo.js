849982accf008ef6ce6ca44f20b1a281ce78681b
Tommy Mallis <tommymallis@gmail.com>
2020-01-29 17:46:51 -0800

    New env set up

--- task-manager/src/middleware/sendFlo.js
+++ task-manager/src/middleware/sendFlo.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const Rpc = require('bitcoin-rpc-async');
 
+const { RPC_USER, RPC_PASS, RPC_HOST, RPC_PORT, WALLET_ADDRESS } = process.env;
+
 const sendFlo = async (floData) => {
   console.log('flodata', floData);
-  const rpcUser = 'user';
-  const rpcPass = 'pass';
-  const rpcHost = process.env.RPCHOST;
-  const rpcPort = '7313'; // 7313 for main net - 17313 for test net
+  const rpcUser = RPC_USER;
+  const rpcPass = RPC_PASS;
+  const rpcHost = RPC_HOST;
+  const rpcPort = RPC_PORT; // 7313 for main net - 17313 for test net
   const rpc = new Rpc(`http://${rpcUser}:${rpcPass}@${rpcHost}:${rpcPort}`);
 
-  const streambedWalletAddress = 'FGbRhT3vTSTiBEe927nispTkNCYAfoYcrm';
+  const streambedWalletAddress = WALLET_ADDRESS;
 
   let paramObj = {
     address: streambedWalletAddress,
